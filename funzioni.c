88a1b7954ef93baa6f7b2fe18a267a46de6bd749
Francesco Cannistrà <francescor.cannistra@davincimilazzo.edu.it>
2025-11-25 18:39:30 +0100

    Add files via upload

---
+++ funzioni.c
@@ -0,0 +1,36 @@
+int addizione() {
+    int a, b;
+    printf("Inserisci il primo numero: ");
+    scanf("%d", &a);
+    printf("Inserisci il secondo numero: ");
+    scanf("%d", &b);
+    return a + b;
+}
+
+int sottrazione() {
+    int a, b;
+    printf("Inserisci il primo numero: ");
+    scanf("%d", &a);
+    printf("Inserisci il secondo numero: ");
+    scanf("%d", &b);
+    return a - b;
+}
+
+int moltiplicazione() {
+    int a, b;
+    printf("Inserisci il primo numero: ");
+    scanf("%d", &a);
+    printf("Inserisci il secondo numero: ");
+    scanf("%d", &b);
+    return a * b;
+}
+
+int divisione() {
+    int a, b;
+    printf("Inserisci il primo numero: ");
+    scanf("%d", &a);
+    printf("Inserisci il secondo numero: ");
+    scanf("%d", &b);
+	return a / b;
+}
+
