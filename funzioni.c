0b4354a5e1ce9be7148accf43b4d536b835f06c9
Francesco Cannistrà <francescor.cannistra@davincimilazzo.edu.it>
2025-12-09 22:02:28 +0100

    Refactor main function to handle user input and operations
    Refactored the main function to include user input for numbers and operation selection, while integrating arithmetic functions.

--- funzioni.c
+++ funzioni.c
@@ -1,36 +1,37 @@
-int addizione() {
-    int a, b;
+#include <stdio.h>
+#include "funzioni.h"
+
+
+
+int main() {
+    char scelta;
+    int ris;
+    int risultato=0;
+    int a;
+    int b;
+   
+    printf("Scegli l'operazione:\n");
+    printf("addizione = Q\nsottrazione = W\nmoltiplicazione = E\ndivisione = S\n");
+    printf("Inserisci la tua scelta: ");
+    scanf(" %c", &scelta); 
+    
+    
     printf("Inserisci il primo numero: ");
     scanf("%d", &a);
     printf("Inserisci il secondo numero: ");
     scanf("%d", &b);
-    return a + b;
-}
 
-int sottrazione() {
-    int a, b;
-    printf("Inserisci il primo numero: ");
-    scanf("%d", &a);
-    printf("Inserisci il secondo numero: ");
-    scanf("%d", &b);
-    return a - b;
-}
+    if (scelta == 'Q' || scelta == 'W' || scelta == 'E') {
+        if (scelta == 'Q')
+            risultato = addizione(a,b);
+        else if (scelta == 'W')
+            risultato = sottrazione(a,b);
+        else if (scelta == 'E')
+            risultato = moltiplicazione(a,b);
 
-int moltiplicazione() {
-    int a, b;
-    printf("Inserisci il primo numero: ");
-    scanf("%d", &a);
-    printf("Inserisci il secondo numero: ");
-    scanf("%d", &b);
-    return a * b;
-}
 
-int divisione() {
-    int a, b;
-    printf("Inserisci il primo numero: ");
-    scanf("%d", &a);
-    printf("Inserisci il secondo numero: ");
-    scanf("%d", &b);
-	return a / b;
-}
+        printf("Risultato: %d\n", risultato);
+    } 
 
+    return 0;
+}
