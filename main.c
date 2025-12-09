51825f8faf473ff502cbab7f3472dc9120ff7bb8
Francesco Cannistrà <francescor.cannistra@davincimilazzo.edu.it>
2025-12-09 22:04:42 +0100

    Modify main to include number inputs for operations
    Added input for two numbers and updated operations to accept parameters.

--- main.c
+++ main.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include "funzioni.h"
 
+
+
 int main() {
     char scelta;
     int ris;
     int risultato=0;
-
+    int a;
+    int b;
+   
     printf("Scegli l'operazione:\n");
     printf("addizione = Q\nsottrazione = W\nmoltiplicazione = E\ndivisione = S\n");
     printf("Inserisci la tua scelta: ");
     scanf(" %c", &scelta); 
+    
+    
+    printf("Inserisci il primo numero: ");
+    scanf("%d", &a);
+    printf("Inserisci il secondo numero: ");
+    scanf("%d", &b);
 
-    if (scelta == 'Q' || scelta == 'W' || scelta == 'E' || scelta == 'S') {
+    if (scelta == 'Q' || scelta == 'W' || scelta == 'E') {
         if (scelta == 'Q')
-            risultato = addizione();
+            risultato = addizione(a,b);
         else if (scelta == 'W')
-            risultato = sottrazione();
+            risultato = sottrazione(a,b);
         else if (scelta == 'E')
-            risultato = moltiplicazione();
-        else if (scelta == 'S')
-            risultato = divisione();
+            risultato = moltiplicazione(a,b);
+
 
         printf("Risultato: %d\n", risultato);
     } 
 
     return 0;
 }
-
