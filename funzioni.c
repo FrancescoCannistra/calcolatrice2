8a39617ed377c1e2d0bdfc8a363d0da7659baab5
Francesco Cannistrà <francescor.cannistra@davincimilazzo.edu.it>
2025-12-09 22:03:12 +0100

    Refactor funzioni.c to include arithmetic functions
    Removed main function and added arithmetic functions.

--- funzioni.c
+++ funzioni.c
@@ -1,37 +1,14 @@
-#include <stdio.h>
-#include "funzioni.h"
 
-
-
-int main() {
-    char scelta;
-    int ris;
-    int risultato=0;
-    int a;
-    int b;
-   
-    printf("Scegli l'operazione:\n");
-    printf("addizione = Q\nsottrazione = W\nmoltiplicazione = E\ndivisione = S\n");
-    printf("Inserisci la tua scelta: ");
-    scanf(" %c", &scelta); 
-    
-    
-    printf("Inserisci il primo numero: ");
-    scanf("%d", &a);
-    printf("Inserisci il secondo numero: ");
-    scanf("%d", &b);
-
-    if (scelta == 'Q' || scelta == 'W' || scelta == 'E') {
-        if (scelta == 'Q')
-            risultato = addizione(a,b);
-        else if (scelta == 'W')
-            risultato = sottrazione(a,b);
-        else if (scelta == 'E')
-            risultato = moltiplicazione(a,b);
-
-
-        printf("Risultato: %d\n", risultato);
-    } 
-
-    return 0;
+int addizione(int a,int b) {
+    return a + b;
 }
+
+int sottrazione(int a,int b) {
+    
+    return a - b;
+}
+
+int moltiplicazione(int a,int b) {
+    return a * b;
+}
+
